8841e5201f622c4d608d6dc010d97b8d3ae50ecc
vijayDVA <48407258+vijayDVA@users.noreply.github.com>
2020-04-10 12:06:09 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/dva/covidtracker/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/dva/covidtracker/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.abdulazizahwan.trackcovid19", appContext.getPackageName());
+        assertEquals("com.dva.covidtracker.back.country", appContext.getPackageName());
     }
 }
